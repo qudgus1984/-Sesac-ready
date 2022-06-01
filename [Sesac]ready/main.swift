633c30bd0c34a48d71966443cefe39ba7f38ff68
ByeongHyeon <qudgus1984@naver.com>
2022-06-01 14:38:23 +0900

    Initial Commit

---
+++ [Sesac]ready/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  [Sesac]ready
+//
+//  Created by 이병현 on 2022/06/01.
+//
+
+import Foundation
+
+var nickname: String = "Cody"
+var age: Int = 25
+let email: String = "qudgus1984@naver.com"
+var rate: Double = 4.5
+var gender: Bool = true
+
+print("안녕하세요 \(nickname)입니다. 저는 \(age)살이고 이메일은 \(email) 입니다.")
