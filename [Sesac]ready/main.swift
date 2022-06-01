17df5e76d3486199f8854e87f986c4304f85559f
ByeongHyeon <qudgus1984@naver.com>
2022-06-01 16:55:19 +0900

    연산자

--- [Sesac]ready/main.swift
+++ [Sesac]ready/main.swift
@@ -23,3 +23,22 @@ age = 22
 gender = false
 rate = 3.5
 print("안녕하세요 \(nickname)입니다. 저는 \(age)살이고 이메일은 \(email) 입니다.")
+
+// WHOAMI
+var birth: Int = 1998
+
+var day: Int = (2022 - birth) * 365
+
+var rice: Int = day * 3
+
+print("""
+        저는\(birth)년에 태어났고,
+        지금까지 \(day)일 째 살고 있습니다
+            n저는 지금까지 \(rice)끼를 먹었어요.
+        그리고 저는 \(day * 8)시간을 잤습니다.
+        """)
+print("저는\(birth)년에 태어났고,")
+print("지금까지 \(day)일 째 살고 있습니다")
+print("n저는 지금까지 \(rice)끼를 먹었어요.")
+print("그리고 저는 \(day * 8)시간을 잤습니다.")
+
