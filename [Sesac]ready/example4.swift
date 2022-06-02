0d3429648d819f893f17f53bde51fad694d7ced6
ByeongHyeon <qudgus1984@naver.com>
2022-06-02 12:29:40 +0900

    배열

---
+++ [Sesac]ready/example4.swift
@@ -0,0 +1,17 @@
+//
+//  example4.swift
+//  [Sesac]ready
+//
+//  Created by 이병현 on 2022/06/02.
+//
+
+import Foundation
+
+// 4. 물 권장 섭취량 : (키 - 몸무게) / 100
+
+//let height: Double = 175
+//let weight: Double = 65
+//
+//let waterGuide = (height + weight) / 100
+//
+//print("하루 물 권장 섭취량은 \(waterGuide)L 입니다.")
