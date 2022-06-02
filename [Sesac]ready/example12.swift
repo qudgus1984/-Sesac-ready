6ef48fa7ee84f5a2d21b5200fc93f7fd8a19fece
ByeongHyeon <qudgus1984@naver.com>
2022-06-02 16:44:03 +0900

    응용

---
+++ [Sesac]ready/example12.swift
@@ -0,0 +1,30 @@
+//
+//  example12.swift
+//  [Sesac]ready
+//
+//  Created by 이병현 on 2022/06/02.
+//
+
+import Foundation
+
+//
+//// 반복문 - 음악 차트
+//
+// let musiclist = ["아이유 - 좋은날", "이무진 - 과제곡", "BTS - Dynamite", "빅뱅 - 봄여름가을겨울"]
+//
+//for music in musiclist {
+//    print("음악 차트 Top3 : \(music)")
+//    print("=======================")
+//}
+//
+//// music -> 반복문 내에서만 쓰는 변수
+//
+//for music in 1...musiclist.count {
+//    print("\(music)위 - \(musiclist[music-1])")
+//
+//}
+//
+////// 1~100
+////for num in 1...100 {
+////    print(num)
+////}
--- [Sesac]ready/main.swift
+++ [Sesac]ready/main.swift
@@ -7,23 +7,63 @@
 
 import Foundation
 
-// 반복문 - 음악 차트
+// 응용 1. 5의 배수와 7의 배수를 찾기
+let randomNumber = [3, 7, 10, 35, 70, 44]
 
- let musiclist = ["아이유 - 좋은날", "이무진 - 과제곡", "BTS - Dynamite"]
-
-for music in musiclist {
-    print("음악 차트 Top3 : \(music)")
-    print("=======================")
+for number in randomNumber {
+    if number % 5 == 0 && number % 7 == 0 {
+        print("\(number)은 5,7의 공배수입니다.")
+    } else if number % 5 == 0 {
+        print("\(number)은 5의 배수입니다.")
+    } else if number % 7 == 0 {
+        print("\(number)은 7의 배수입니다.")
+    } else {
+        print("\(number)은 5,7의 배수가 아닙니다.")
+    }
 }
 
-// music -> 반복문 내에서만 쓰는 변수
+// 응용 2. 우수자 선발하기(300점 만점)
 
-for music in 1...3 {
-    print("\(music)위 - \(musiclist[music-1])")
+let scoreArray: [Int] = [180, 270, 240, 300, 90, 120]
 
+var bestArray: [Int] = []
+for score in scoreArray {
+    if score >= 200 {
+        print("\(score)점으로 우수자입니다.")
+        bestArray.append(score)
+    } else {
+        print("\(score)점으로 우수자가 아닙니다.")
+    }
 }
 
-//// 1~100
-//for num in 1...100 {
-//    print(num)
-//}
+print(bestArray)
+
+print("우수자는 총 \(bestArray.count)명 입니다.")
+
+if bestArray.contains(300) {
+    print("만점자가 있습니다.")
+}
+
+let voteArray = ["명수","준하","명수","명수","명수","명수","준하","준하","준하","준하"]
+var voteMS = 0
+var voteJH = 0
+
+for vote in voteArray {
+    if vote == "명수" {
+        voteMS += 1
+    } else {
+        voteJH += 1
+    }
+}
+var presedent: String
+if voteMS > voteJH {
+    presedent = "명수"
+} else if voteMS < voteJH {
+    presedent = "준하"
+} else {
+    presedent = "재투표"
+}
+print("총 투표수는 \(voteArray.count)이며, 명수는 \(voteMS)표, 준하는 \(voteJH)를 받았습니다.\n 이에따라 대통령은 \(presedent)입니다. ")
+
+
+// 응용 4. 국가별 영화 출력
