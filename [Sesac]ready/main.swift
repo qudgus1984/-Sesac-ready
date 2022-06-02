4b7f335d90ebb0a79abf4c739cabd37e5cd323c1
ByeongHyeon <qudgus1984@naver.com>
2022-06-02 14:46:20 +0900

    조건문

--- [Sesac]ready/main.swift
+++ [Sesac]ready/main.swift
@@ -11,7 +11,7 @@ import Foundation
 // 조건문 (if, switch)
 var gender = true
 
-if gender == true {
+if gender {
     print("저는 남자입니다.")
 } else {
     print("저는 여자입니다.")
@@ -19,20 +19,22 @@ if gender == true {
 
 print("저는 \(gender)입니다.")
 
-var weight = Int(readLine()!)!
+var weight = Int.random(in: 1...20)
 
 if weight > 15 {
-    print("수화물요금 내야합니다.")
+    print("무게가 \(weight)로,수화물요금 내야합니다.")
 } else {
-    print("공짜")
+    print("무게가 \(weight)이므로 공짜")
 }
 
 // 부모님 동의가 필요한 회원가입
 
-var age = 50
+var age = Int.random(in: 1...50)
 
 if age >= 14 {
-    print("부모님 동의 없이 가입할 수 있습니다.")
+    print("나이가 \(age)이므로 부모님 동의 없이 가입할 수 있습니다.")
+} else {
+    print("나이가 \(age)이므로 부모님 동의가 필요합니다.")
 }
 
 // 영화 프로그램 추천
@@ -40,6 +42,8 @@ var userSelect = "영화"
 
 if userSelect != "영화" {
     print("오늘의 드라마 '어게인 마이 라이프'를 추천드립니다.")
+} else {
+    print("오늘의 드라마 '범죄도시2' 를 추천드립니다")
 }
 
 // 성적 장학금
@@ -48,6 +52,8 @@ var gradePoint = 4.4
 
 if gradePoint > 4.3 {
     print("장학금을 받을 수 있습니다.")
+} else {
+    print("장학금 못받아요 ㅠㅠ")
 }
 
 
